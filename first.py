1408e7174f918607275649bab06020ee31de380a
amren1254 <amren1254@gmail.com>
2019-08-31 00:47:08 +0530

    NB: making compatible to python3

--- first.py
+++ first.py
@@ -15,13 +15,13 @@ def login():
     read=csv.reader(open('n1.csv'))   # CSV file is opened in the read mode....
     for row in read:
         data.append(row)
-    uname=raw_input('Enter your username:')
+    uname=input('Enter your username:')
     for i in range(0,len(data)):
         if uname==data[i][4]:
             b=i
             
             print ('Correct Username')
-            passw=raw_input('Enter your password')
+            passw=input('Enter your password')
             if passw==data[b][5]:
                 print ('Correct Password\n'),profile(b)     # Profile function is called with an argument....
             else:
@@ -31,13 +31,13 @@ def login():
 
 def signup():
     print ("\n------ Welcome to the Signup -------"),time.sleep(2)    # Time attribute sleep is called to delay for 2 seconds....
-    name=raw_input('Enter your name')
-    age=raw_input('\nEnter your age')
-    address=raw_input('\n Enter your address')
-    email=raw_input('\n Enter your email')
-    username=raw_input('Enter your username')
-    password=raw_input('\n Enter your password')
-    first=raw_input('How much do you want to credit as your initial balance(min. 3000)')
+    name=input('Enter your name')
+    age=input('\nEnter your age')
+    address=input('\n Enter your address')
+    email=input('\n Enter your email')
+    username=input('Enter your username')
+    password=input('\n Enter your password')
+    first=input('How much do you want to credit as your initial balance(min. 3000)')
     print ('\n _____New user created----')
     f1=open('n1.csv','a')           # CSV file is opened in append mode to write all the data of new user....
 ##  Write operation is performed in csv file....
@@ -64,7 +64,7 @@ def signup():
 
 def home():
     print ('\n\t\t::::::Welcome :::::')
-    ch=input('\t\t\nEnter your choice \n 1.Login\n\n 2.Signup\n\n 3.Exit')
+    ch=int(input('\t\t\nEnter your choice \n 1.Login\n\n 2.Signup\n\n 3.Exit'))
     if(ch==1):
      login()                 # Login function is called without any argument....
     elif(ch==2):
@@ -78,7 +78,7 @@ def home():
 
 def credit(r):
     print ('\n\t\t---Welcome to credit panel----'),time.sleep(2)
-    cred= int(raw_input('\n\t\t Enter the amount you want to credit'))
+    cred= int(input('\n\t\t Enter the amount you want to credit'))
     data=[]
     read=csv.reader(open('n1.csv'))         # CSV file is opened in the read mode....
     for row in read:
@@ -117,7 +117,7 @@ def debit(r):
 
 def transfer(crr):
     print ('Welcome to the transfer panel')
-    acc=raw_input('Enter the username of whom you want to transfer')
+    acc=input('Enter the username of whom you want to transfer')
     data=[]
     read=csv.reader(open('n1.csv'))
     for row in read:
@@ -128,7 +128,7 @@ def transfer(crr):
             print ('User Found \t\t'),data[a][0],('\n\nYou can proceed')
 ##        else:
 ##            print 'Specified user not found'
-    amt=int(raw_input('Enter amount to be transferred'))
+    amt=int(input('Enter amount to be transferred'))
     data[a][6]=eval(data[a][6])+amt
     data[crr][6]=eval(data[crr][6])-amt
     f1=open('n1.csv','w')
@@ -141,7 +141,7 @@ def transfer(crr):
 # Logout module for logout the user....
 
 def logout():
-    ch= raw_input('\nDo you want to logout(y/n)')
+    ch= input('\nDo you want to logout(y/n)')
     if ch=='y' or ch=='Y':
         f1=open('n1.csv','r')
         f1.close()

982ecc92d1772d6a9142147fa12544bf67d1e1cc
amren1254 <amren1254@gmail.com>
2019-09-08 16:10:21 +0530

    NB: Inserted data to the database

--- net_banking.py
+++ net_banking.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 '''This is the new version of formerly created first.py in which 
 I am trying to write the same software which will run on python3 '''
 
@@ -7,26 +8,31 @@ import csv      # importing csv library as used in older version of code to stor
 #but I am using postgresql database in this one to store user data.
 import psycopg2 # importing psycopg2 driver library to make connection with postgres database.
 from termcolor import colored
+import webbrowser
+
+import os
 
 
-#handling connection to the postgres database
-try:
-    conn = psycopg2.connect(user = "amren",
-            password = "amren",
-            host = "localhost",
-            port = "5432",
-            database = "tutree")
-    cursor = conn.cursor()      #creating cursor for the database to perform operation on database 
-    cursor.execute("select version()")  #executing the first query
-    record = cursor.fetchone()
-    print("You are connected to ",record,"\n")  #printed the first query result
-except(Exception,psycopg2.Error) as error:
-    print("Error while connecting to database")
-finally:
-    if(conn):
-        cursor.close()      #closing connection to database
-        conn.close()
-        print(colored("Postgresql connection closed","red"))
+def connection():
+    try:
+        conn = psycopg2.connect(user = "amren",
+                password = "amren",
+                host = "localhost",
+                port = "5432",
+                database = "tutree")
+        cursor = conn.cursor()      #creating cursor for the database to perform operation on database 
+        #cursor.execute("select version()")  #executing the first query
+               #record = cursor.fetchone()
+        #print("You are connected to ",record,"\n")  #printed the first query result
+    except(Exception,psycopg2.Error) as error:
+        print(colored("Error while connecting to database","red"))
+    finally:
+        if(conn):
+            cursor.close()      #closing connection to database
+            conn.close()
+            print(colored("Postgresql connection closed","red"))
+
+
 
 #profile function for a user which will show user details
 def profile(userid):
@@ -67,17 +73,23 @@ def signup():
     name = input("\tEnter you Name\t\t")
     email = input("\tEnter your Email\t")
     password = input("\tCreate a password\t")
-    query = "insert into user_data values (name,email,password)"
-    #query.execute()
-    
+    #query = "insert into netbanking values (1,name,email,password)"
+    #handling connection to the postgres database
+    connection()
+    sql = "insert into netbanking values(%s,%s,%s)"
+    record_to_insert = (name,email,password)
+    cursor.execute(sql,record_to_insert)
+    conn.commit()
 
 
 #about function to let user know about this project
 def about():
+    print(colored("You will be redirected to your default browser in 6 sec",'yellow',attrs = ['blink']))
     print('''\tThis is a practice project before some exam 
             I just want to have hands on experience on python before
             executing python script file in real environment.\n''')
-
+    time.sleep(6)
+    webbrowser.open_new("about_netbanking.html")
 
 
 
